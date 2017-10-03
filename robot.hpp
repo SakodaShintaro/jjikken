d14b06f048d847a847764a3d49cc5c5455805e92
Sakoda Shintaro <rgbygscrsedppbwg@gmail.com>
2017-10-03 14:35:47 +0900

    方向定数使っていろいろやるように変更中

--- robot.hpp
+++ robot.hpp
@@ -4,11 +4,17 @@
 class Robot {
 public:
     Robot();
-    void goForward(int speed = 1000);
-    void back(int speed = 1000);
+    //移動類
+    //方向を示す定数
+    enum Direction {
+        FORWARD, BACK, RIGHT, LEFT
+    };
+    void run(Direction direction, int speed = default_speed);
     void stop();
-    void stopAndTurnRight();
-    void stopAndTurnLeft();
+
+    //90度,180度方向転換するイメージだけど、角度はどうやって測るのかわからない
+    void stopAndTurn(Direction direction);
+    void curve
     void speedUp();
     void speedDown();
 
@@ -20,20 +26,39 @@ private:
     //左右の両輪
     Wheel right_wheel_(true), left_wheel(false);
 
-    //カメラ入力
+    static int default_speed = 10000;
+
+    //カメラ
     //openCVを使うはず
+    //うーん、どういう設計になるのかうまく創造できてない……
+
+    //マイク
 
     //センサ類
 };
 
-void Robot::goForward(int speed) {
-    right_wheel.run(speed);
-    left_wheel.run(speed);
-}
-
-void Robot::back(int speed) {
-    right_wheel.run(-speed);
-    left_wheel.run(-speed);
+void Robot::run(Direction direction, int speed) {
+    switch (direction) {
+    case FORWARD:
+        right_wheel.run(speed);
+        left_wheel.run(speed);
+        break;
+    case BACK:
+        right_wheel.run(-speed);
+        left_wheel.run(-speed);
+        break;
+    case RIGHT:
+        stopAndTurn(RIGHT);
+        run(FORWARD, speed);
+        break;
+    case LEFT:
+        stopAndTurn(LEFT);
+        run(FORWARD, speed);
+        break;
+    default:
+        //ここにはこないはず
+        assert(false);
+    }
 }
 
 void Robot::stop() {
@@ -41,5 +66,16 @@ void Robot::stop() {
     left_wheel.gradualStop();
 }
 
-void Robot::turnRight() {
+void Robot::stopAndTurn(Direction direction) {
+    assert(direction == RIGHT || direction == LEFT);
+    switch (direction) {
+    case RIGHT:
+        right_wheel.run(-default_speed / 2);
+        left_wheel.run(+default_speed / 2);
+        break;
+    case LEFT:
+        right_wheel.run(+default_speed / 2);
+        left_wheel.run(-default_speed / 2);
+        break;
+    }
 }
